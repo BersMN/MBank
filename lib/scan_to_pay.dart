6a2233b921c478f5a84ffcc82f190a6ab4f372f5
Bers <yansan0601@gmail.com>
2025-05-16 19:21:36 +0800

    Update scan_to_pay.dart

--- lib/scan_to_pay.dart
+++ lib/scan_to_pay.dart
@@ -11,10 +11,65 @@ class _ScanToPayScreenState extends State<ScanToPayScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xFF304FFE),
       appBar: AppBar(
+        backgroundColor: Color(0xFF304FFE),
         leading: IconButton(
-          icon: Icon(Icons.keyboard_arrow_left),
+          icon: Icon(Icons.keyboard_arrow_left, color: Colors.white),
           onPressed: () => Navigator.of(context).pop(),
+        ),
+      ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Center(
+              child: Container(
+                width: MediaQuery.of(context).size.width * .5,
+                height: MediaQuery.of(context).size.width * .5,
+                color: Colors.grey.shade200,
+                margin: const EdgeInsets.symmetric(vertical: 48),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 16.0),
+              child: Text(
+                "Scan a QR to Pay",
+                style: TextStyle(color: Colors.white, fontSize: 20),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 32.0),
+              child: Text(
+                "Hold the code inside the frame, it will be scanned automatically",
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+            Spacer(),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.flash_on, color: Colors.white),
+                  Container(
+                    width: 50,
+                    height: 50,
+                    margin: const EdgeInsets.symmetric(horizontal: 32),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      shape: BoxShape.circle,
+                      border: Border.all(
+                        color: Colors.blue.withAlpha(100),
+                        width: 6,
+                      ),
+                    ),
+                  ),
+                  Icon(Icons.rotate_90_degrees_cw_rounded, color: Colors.white),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
