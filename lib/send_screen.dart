55717cf479198fb9f02be8f07ee4ba63e4b3ebd2
Bers <yansan0601@gmail.com>
2025-05-16 16:35:00 +0800

    Create send_screen.dart

---
+++ lib/send_screen.dart
@@ -0,0 +1,259 @@
+import 'package:flutter/material.dart';
+
+class SendScreen extends StatelessWidget {
+  const SendScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        leading: IconButton(
+          icon: Icon(Icons.keyboard_arrow_left),
+          onPressed: () => Navigator.of(context).pop(),
+        ),
+      ),
+      backgroundColor: Colors.white.withAlpha(240),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Choose Recipient',
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+              Text(
+                'Please select your recipient to send money.',
+                style: TextStyle(color: Colors.grey),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+                padding: const EdgeInsets.all(16),
+                margin: const EdgeInsets.only(top: 20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      height: 40,
+                      child: TextField(
+                        style: TextStyle(fontSize: 14),
+                        decoration: InputDecoration(
+                          contentPadding: EdgeInsets.only(right: 12),
+                          fillColor: Colors.grey.withAlpha(20),
+                          filled: true,
+                          prefixIcon: Icon(Icons.search, color: Colors.grey),
+                          labelText: "Search `Recipient Email`",
+                          labelStyle: TextStyle(
+                            fontSize: 12.0,
+                            color: Colors.grey.shade400,
+                          ),
+                          floatingLabelBehavior: FloatingLabelBehavior.never,
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.transparent),
+                            borderRadius: BorderRadius.circular(32.0),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.transparent),
+                            borderRadius: BorderRadius.circular(32.0),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 8.0),
+                      child: Text("Most Recent"),
+                    ),
+                    InkWell(
+                      onTap:
+                          () => Navigator.pushNamed(context, '/selectpurpose'),
+                      child: Row(
+                        children: [
+                          Container(
+                            height: 40,
+                            width: 40,
+                            margin: const EdgeInsets.only(right: 8),
+                            decoration: BoxDecoration(
+                              color: Colors.grey.shade200,
+                              shape: BoxShape.circle,
+                            ),
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "Mehedi Hasan",
+                                style: TextStyle(fontSize: 12),
+                              ),
+                              Text(
+                                "helloyouthmind@ghmail.com",
+                                style: TextStyle(
+                                  fontSize: 12,
+                                  color: Colors.grey.shade600,
+                                ),
+                              ),
+                            ],
+                          ),
+                          Spacer(),
+                          Text("-\$100", style: TextStyle(color: Colors.red)),
+                        ],
+                      ),
+                    ),
+                    Divider(color: Colors.grey.shade200, height: 24),
+                    InkWell(
+                      onTap:
+                          () => Navigator.pushNamed(context, '/selectpurpose'),
+                      child: Row(
+                        children: [
+                          Container(
+                            height: 40,
+                            width: 40,
+                            margin: const EdgeInsets.only(right: 8),
+                            decoration: BoxDecoration(
+                              color: Colors.grey.shade200,
+                              shape: BoxShape.circle,
+                            ),
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "Mehedi Hasan",
+                                style: TextStyle(fontSize: 12),
+                              ),
+                              Text(
+                                "helloyouthmind@ghmail.com",
+                                style: TextStyle(
+                                  fontSize: 12,
+                                  color: Colors.grey.shade600,
+                                ),
+                              ),
+                            ],
+                          ),
+                          Spacer(),
+                          Text("-\$100", style: TextStyle(color: Colors.red)),
+                        ],
+                      ),
+                    ),
+                    Divider(color: Colors.grey.shade200, height: 24),
+                    InkWell(
+                      onTap:
+                          () => Navigator.pushNamed(context, '/selectpurpose'),
+                      child: Row(
+                        children: [
+                          Container(
+                            height: 40,
+                            width: 40,
+                            margin: const EdgeInsets.only(right: 8),
+                            decoration: BoxDecoration(
+                              color: Colors.grey.shade200,
+                              shape: BoxShape.circle,
+                            ),
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "Mehedi Hasan",
+                                style: TextStyle(fontSize: 12),
+                              ),
+                              Text(
+                                "helloyouthmind@ghmail.com",
+                                style: TextStyle(
+                                  fontSize: 12,
+                                  color: Colors.grey.shade600,
+                                ),
+                              ),
+                            ],
+                          ),
+                          Spacer(),
+                          Text("-\$100", style: TextStyle(color: Colors.red)),
+                        ],
+                      ),
+                    ),
+                    Divider(color: Colors.grey.shade200, height: 24),
+                    InkWell(
+                      onTap:
+                          () => Navigator.pushNamed(context, '/selectpurpose'),
+                      child: Row(
+                        children: [
+                          Container(
+                            height: 40,
+                            width: 40,
+                            margin: const EdgeInsets.only(right: 8),
+                            decoration: BoxDecoration(
+                              color: Colors.grey.shade200,
+                              shape: BoxShape.circle,
+                            ),
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "Mehedi Hasan",
+                                style: TextStyle(fontSize: 12),
+                              ),
+                              Text(
+                                "helloyouthmind@ghmail.com",
+                                style: TextStyle(
+                                  fontSize: 12,
+                                  color: Colors.grey.shade600,
+                                ),
+                              ),
+                            ],
+                          ),
+                          Spacer(),
+                          Text("-\$100", style: TextStyle(color: Colors.red)),
+                        ],
+                      ),
+                    ),
+                    Divider(color: Colors.grey.shade200, height: 24),
+                    InkWell(
+                      onTap:
+                          () => Navigator.pushNamed(context, '/selectpurpose'),
+                      child: Row(
+                        children: [
+                          Container(
+                            height: 40,
+                            width: 40,
+                            margin: const EdgeInsets.only(right: 8),
+                            decoration: BoxDecoration(
+                              color: Colors.grey.shade200,
+                              shape: BoxShape.circle,
+                            ),
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "Mehedi Hasan",
+                                style: TextStyle(fontSize: 12),
+                              ),
+                              Text(
+                                "helloyouthmind@gmail.com",
+                                style: TextStyle(
+                                  fontSize: 12,
+                                  color: Colors.grey.shade600,
+                                ),
+                              ),
+                            ],
+                          ),
+                          Spacer(),
+                          Text("-\$100", style: TextStyle(color: Colors.red)),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
